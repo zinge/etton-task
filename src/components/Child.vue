121117a7d6a1653d49bfa77691c2d46d949783a5
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-10-31 17:04:54 +0300

    init

---
+++ src/components/Child.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="child">
+    <h3>This child component</h3>
+    <ul>
+      <li v-for="(value, index) in user" :key="index.id">
+        {{'index: ' + index + ', value: ' + value}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+
+  props: ['user'],
+
+}
+</script>
---
+++ src/components/Parent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="parent">
+    <h3>This parent component</h3>
+    <p>Enter you name</p>
+    <input type="text" v-model="user.name"></input><br>
+    <p>Enter you age</p>
+    <input type="number" v-model="user.age"></input><br>
+    <p>Enter you phone</p>
+    <input type="tel" v-model="user.phone"></input><br>
+
+    <child :user="user"/>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+
+export default {
+  name: 'Parent',
+
+  data: () => ({
+    user: {
+      name: '',
+      age: '',
+      phone: ''
+    }
+  }),
+
+  components: {
+    Child
+  }
+}
+</script>
+
